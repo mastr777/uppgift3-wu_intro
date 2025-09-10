bac4c263dd9eb2ebf5c87eeb8d83ed36161686ff
mastr777 <mstrandberg777@gmail.com>
2025-09-10 11:39:36 +0200

    Added a page Reload function

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function myClicker() {
   document.getElementById("changeText").innerHTML = "Goodbye World. ."; 
   document.getElementById("nameAppears").innerHTML = "Mattias Strandberg";
 
-  setTimeout(function() { location.reload(); }, 5000 );
+  setTimeout(function() { location.reload(); }, 6000 );
 };

a8fb578369c83c08dd9bc2b3f421c4cabd2ae561
mastr777 <mstrandberg777@gmail.com>
2025-09-10 11:48:20 +0200

    Added Refresh text and Reload function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function myClicker() {
   document.getElementById("changeText").innerHTML = "Goodbye World. ."; 
   document.getElementById("nameAppears").innerHTML = "Mattias Strandberg";
-  document.getElementById("refreshText").innerHTML = "Site will be refreshed in 6 seconds. .";
+  document.getElementById("refreshText").innerHTML = "(site will be refreshed in 7 seconds. .)";
 
-  setTimeout(function() { location.reload(); }, 6000 );
+  setTimeout(function() { location.reload(); }, 7000 );
 };

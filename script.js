6344759bef9d207cfcace184b94a6da8c36febb0
mastr777 <mstrandberg777@gmail.com>
2025-09-15 20:40:46 +0200

    Changed DOM syntax in function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function myClicker() {
-  //* document.getElementById("changeText").innerHTML = "Goodbye World. ."; *//
+ 
   document.querySelector("#changeText").innerHTML = "Goodbye World. ."; 
   document.querySelector("#nameAppears").innerHTML = "Mattias Strandberg";
   document.querySelector("#refreshText").innerHTML = "(site will be refreshed in 7 seconds. .)";

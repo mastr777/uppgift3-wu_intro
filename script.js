66dcb7e26e40bdf869c175c2d2d66b17eea2a5a1
mastr777 <mstrandberg777@gmail.com>
2025-09-15 23:36:17 +0200

    Added querySelector / EventListener version

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ function myClicker() {
 
   setTimeout(function() { location.reload(); }, 7000 );
 };
+
+// ---------------------------------------------------------------------------------------
+
+
+// Added querySelector / EventListener version
+
+
+/* const changeText = document.querySelector("#changeText");
+const nameAppears = document.querySelector("#nameAppears");
+const refreshText = document.querySelector("#refreshText");
+
+changeText.addEventListener("click", myClicker);
+nameAppears.addEventListener("click", myClicker);
+refreshText.addEventListener("click", myClicker);
+
+function myClicker() {
+  changeText.textContent = "Goodbye World. .";
+
+  nameAppears.textContent = "Mattias Strandberg";
+
+  refreshText.textContent = "(site will be refreshed in 7 seconds. .)";
+
+  setTimeout(function() { location.reload(); }, 7000 );
+}; */

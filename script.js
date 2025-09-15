f8f0256a370d76b4ad159797ac5384c2016eeb48
mastr777 <mstrandberg777@gmail.com>
2025-09-15 20:29:49 +0200

    Changed DOM syntax

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function myClicker() {
-  document.getElementById("changeText").innerHTML = "Goodbye World. ."; 
-  document.getElementById("nameAppears").innerHTML = "Mattias Strandberg";
-  document.getElementById("refreshText").innerHTML = "(site will be refreshed in 7 seconds. .)";
+  //* document.getElementById("changeText").innerHTML = "Goodbye World. ."; *//
+  document.querySelector("#changeText").innerHTML = "Goodbye World. ."; 
+  document.querySelector("#nameAppears").innerHTML = "Mattias Strandberg";
+  document.querySelector("#refreshText").innerHTML = "(site will be refreshed in 7 seconds. .)";
 
   setTimeout(function() { location.reload(); }, 7000 );
 };
